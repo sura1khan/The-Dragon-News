9b800d54a404404fc3e978f2f3c33d6b78b904ad
Suraia <khansuraia645@gmail.com>
2025-05-04 23:49:37 +0600

    total news

--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -1,15 +1,22 @@
 
+import { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
     const {id} = useParams();
     const data = useLoaderData();
-
-    console.log(id,data);
+ const [categoryNews, setCategoryNews] = useState([])
+    // console.log(id,data);
+    useEffect(()=>{
+        const filteredNews = data.filter((news)=>news.category_id== id);
+     console.log(filteredNews);
+     setCategoryNews(filteredNews);
+    },[data,id]);
+   
     
     return (
         <div>
-            CategoryNews - {id}
+           total {categoryNews.length} News Found
         </div>
     );
 };
